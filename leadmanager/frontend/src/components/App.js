b4f3762cd411aec6e7ffbe9bf36355509ad625e0
arecastudio <ondiisrail@gmail.com>
2020-06-17 23:20:03 +0900

    third video

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 
+import {Provider} from 'react-redux';
+import store from '../store';
+
 class App extends Component {
-  constructor() {
-    super();
-  }
-  render(){
-    return(
-      <Fragment>
-        <Header/>
-        <div className="container">
-          <Dashboard/>
-        </div>
-      </Fragment>
-    )
-  }
+    constructor() {
+	super();
+    }
+    render(){
+	return(
+	    <Provider store={store}>
+	      <Fragment>
+		<Header/>
+		<div className="container">
+		  <Dashboard/>
+		</div>
+	      </Fragment>
+	    </Provider>
+	)
+    }
 }
 
 ReactDOM.render(<App/>,document.getElementById('app'));
